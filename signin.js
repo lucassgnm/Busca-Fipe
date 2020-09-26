48a234fc7fe52a68408a0352b5d21e9fc312b53c
Lucas Giovani <lucasgnm@gmail.com>
2020-09-26 15:39:44 -0300

    iniciando desenvolvimento da página de cadastro

---
+++ signin.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", () => {
+    /* Configs */
+    /* Adicionar padding top para mostrar o conteúdo atrás da barra de navegação */
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+    /* Detectar rolagem para cima ou para baixo */
+    if ($('.smart-scroll').length > 0) { /* Verifica se o elemento existe */
+        var last_scroll_top = 0;
+        $(window).on('scroll', function() {
+            scroll_top = $(this).scrollTop();
+            if (scroll_top < last_scroll_top) {
+                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+            } else {
+                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+    }
+});
+
+$(document).ready(function() {
+
+    var current_fs, next_fs, previous_fs;
+
+    // No BACK button on first screen
+    if ($(".show").hasClass("first-screen")) {
+        $(".prev").css({ 'display': 'none' });
+    }
+
+    // Next button
+    $(".next-button").click(function() {
+
+        current_fs = $(this).parent().parent();
+        next_fs = $(this).parent().parent().next();
+
+        $(".prev").css({ 'display': 'block' });
+
+        $(current_fs).removeClass("show");
+        $(next_fs).addClass("show");
+
+        $("#progressbar li").eq($(".card2").index(next_fs)).addClass("active");
+
+        current_fs.animate({}, {
+            step: function() {
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+
+                next_fs.css({
+                    'display': 'block'
+                });
+            }
+        });
+    });
+
+    // Previous button
+    $(".prev").click(function() {
+
+        current_fs = $(".show");
+        previous_fs = $(".show").prev();
+
+        $(current_fs).removeClass("show");
+        $(previous_fs).addClass("show");
+
+        $(".prev").css({ 'display': 'block' });
+
+        if ($(".show").hasClass("first-screen")) {
+            $(".prev").css({ 'display': 'none' });
+        }
+
+        $("#progressbar li").eq($(".card2").index(current_fs)).removeClass("active");
+
+        current_fs.animate({}, {
+            step: function() {
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+
+                previous_fs.css({
+                    'display': 'block'
+                });
+            }
+        });
+    });
+
+});
