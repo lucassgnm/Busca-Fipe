5e943ec665db49437b01053693ca03ebfaaa88cc
Lucas Giovani <lucasgnm@gmail.com>
2020-09-28 17:48:04 -0300

    finalizando projeto

---
+++ porque.js
@@ -0,0 +1,10 @@
+/* Muda o aspecto do botão quando ativo */
+$('.btn').on('click', function() {
+    $('.btn').removeClass('active');
+    $(this).addClass('active');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+});
